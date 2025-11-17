711bf53bdcc78217a29a518104ead8dca9114e3e
Lincoln K. <104798220+LincolnKermit@users.noreply.github.com>
2025-11-17 14:12:46 +0100

    Enhance process handling and output to HTML
    Retrieve process information, filter and sort by CPU usage, and output to HTML.

--- objects_handling.ps1
+++ objects_handling.ps1
@@ -1,1 +1,12 @@
-
+$proc = Get-Process
+$proc | Get-Member
+$proc | Select-Object Name, CPU, WorkingSet
+$proc | Select-Object Name, CPU, WorkingSet | Where-Object {$proc.WorkingSet -gt 100MB}
+$proc | Select-Object Name, CPU, WorkingSet | Where-Object {$proc.WorkingSet -gt 100MB} | Sort-Object -Property CPU -Descending
+$object=[PSCustomObject]@{
+  NomProcessus = $proc[0].Name
+  MemMB = $proc[0].WorkingSet
+  CPUSeconds = $proc[0].CPU
+}
+$object | ConvertTo-Html | out-file file.html
+.\file.html
